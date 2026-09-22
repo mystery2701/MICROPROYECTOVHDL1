d5859816d3ed8b44c6795814ace25ba9e9d12b4b
mystery2701 <emanuellaporte@unicauca.edu.co>
2026-09-22 07:02:57 -0500

    correcion en codigo

--- contador.vhd
+++ contador.vhd
@@ -3,19 +3,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity contador is
     Port (
-        valor : in  integer range 0 to 9  -- ERROR AQUÍ
-        seg   : out STD_LOGIC_VECTOR(6 downto 0)
+        clk_50mhz      : in  STD_LOGIC;
+        reset          : in  STD_LOGIC;
+        alarma_led     : out STD_LOGIC
     );
 end contador;
 
-architecture Comportamental of contador is
+architecture Estructural of contador is
+
+    
+    component contador is
+        Port ( 
+            clk        : in  STD_LOGIC; 
+            reset      : in  STD_LOGIC; 
+            alarma_led : out STD_LOGIC 
+        );
+    end component;
+
+    signal clk_1hz_int : STD_LOGIC := '0';
+
 begin
-    process(valor)
-    begin
-        case valor is
-            when 0 => seg <= "1000000"; 
-            when 1 => seg <= "1111001"; 
-            when others => seg <= "1111111"; 
-        end case;
-    end process;
-end Comportamental;
+    -- ERROR AQUÍ en el port map: Intentando mapear un nombre que no está en el componente
+    MODULO_CONTROL: control_tiempos port map (
+        clk_1hz    => clk_1hz_int, 
+        reset      => reset,
+        alarma_led => alarma_led
+    );
+end Estructural;
